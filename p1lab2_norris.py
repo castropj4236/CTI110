e0686c1e953a94fed9819666911261d88e83a7a7
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-03 17:59:17 +0000

    p1lab2 started

---
+++ p1lab2_norris.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1LAB2 - Selling Things
+# castropj
+# 9/3/26
+
+# Fictional Store -- pick three things
+# product_name, product_count, product_price
+
+# Change these to your own values
+product_name = "Television"
+product_count = 24
+product_price = 399.99
+
+print("Welcome to the", product_name, "store")
+print("We have", product_count, product_name, "(s) at $", product_price,"each")
+
+total = product_count * product_price
+
+print("Total is: $",total)
