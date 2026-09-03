b4ac2c145326bc47098bc015bb9176e1259c0659
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-03 18:19:28 +0000

    p1lab2 started

--- p1lab2_norris.py
+++ p1lab2_norris.py
@@ -6,14 +6,30 @@
 # Fictional Store -- pick three things
 # product_name, product_count, product_price
 
-# Change these to your own values
-product_name = "Television"
-product_count = 24
-product_price = 399.99
+# Hardcoding sets values directly.
+# product_name = "Television"
+# product_count = 24
+# product_price = 399.99
 
+# Instead, we ask the user with input()
+# INPUT
+print("STORE STARTUP")
+print("_" * 10) # ten _ in a row
+product_name = input("Enter product name: ")
+product_count = input("Enter product count: ")
+product_price = input("Enter unit price: ")
+
+# PROCESSING
+product_count = int(product_count)
+product_price = float(product_price)
+total = product_count * product_price
+
+
+# OUTPUT
+print("CUSTOMER INTERFACE")
+print("_" * 10) # ten _ in a row
 print("Welcome to the", product_name, "store")
 print("We have", product_count, product_name, "(s) at $", product_price,"each")
 
-total = product_count * product_price
 
 print("Total is: $",total)
