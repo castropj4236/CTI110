61c8ff536d86801d58ffa3b93dafe41b3621e06f
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-03 18:33:36 +0000

    p1lab2 Finished

--- p1lab2_castroperez.py
+++ p1lab2_castroperez.py
@@ -24,12 +24,11 @@ product_count = int(product_count)
 product_price = float(product_price)
 total = product_count * product_price
 
-
 # OUTPUT
 print("CUSTOMER INTERFACE")
 print("_" * 10) # ten _ in a row
 print("Welcome to the", product_name, "store")
-print("We have", product_count, product_name, "(s) at $", product_price,"each")
+# For later -- f string with {variable:.2f} is the magic word to get 2 decimals
+print(f"We have, {product_count}, {product_name}(s) at ${product_price:.2f},each")
+print(f"Total is: ${total:.2f}.")
 
-
-print("Total is: $",total)
