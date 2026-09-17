3912dc46e7b2f753ee1837ca5378d43428d29810
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-17 18:13:03 +0000

    P2HW1 ended

--- p2hw1_castroperez.py
+++ p2hw1_castroperez.py
@@ -3,12 +3,25 @@
 # Talk about how to format the display
 
 # Sample data - real program uses input()
-destination = "Atlanta"
-budget      = 5000
-expenses    = 1000
-remaining   = budget - expenses
+budget = int(input("enter budget: "))
 
-print(f"{"Destination:":<15} {destination:<15}")
-print(f"{"Budget:":<15} ${budget:<15.2f}")
-print(f"{"Expenses:":<15} ${expenses:<15.2f}")
-print(f"{"Remaining:":<15} ${remaining:<15.2f}")
+destination = input("enter your destination: ")
+
+gas = int(input("enter that you can spend in gas: "))
+
+hotel = int(input("enter that you spend the night: "))
+
+food = int(input("enter the cost of the food and snacks: "))
+
+expenses = gas+hotel+food
+
+remaining = budget-expenses
+
+print("<<<<<<<<<<Travel-Expenses>>>>>>>>>>")
+print(f"{"Location:":<15} {destination:<15}")
+print(f"{"Initial Budget:":<15} ${budget:<15.2f}")
+print(f"{"Fuel:":<15} ${gas:<15.2f}")
+print(f"{"Accomodation:":<15} ${gas:<15.2f}")
+print(f"{"Food:":<15} ${food:<15.2f}")
+print("<<<<<<<<<<Total Budget>>>>>>>>>>")
+print(f"{"Remaining Balance for Budget:":<15} ${remaining:<15.2f}")
---
+++ p2hw2_castroperez.py
@@ -0,0 +1,3 @@
+# CTI 110
+# P2HW2 - Just Setup
+# Talk about how to format the display
