5ced23dbf22c891595c4a698cdd1e9567b17b989
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-17 18:26:53 +0000

    P2HW2 unfinished

--- p2hw2_castroperez.py
+++ p2hw2_castroperez.py
@@ -1,3 +1,13 @@
 # CTI 110
-# P2HW2 - Just Setup
-# Talk about how to format the display
+# P2HW2 - just the setup
+
+# This example only uses three numbers, the full uses six.
+# get the grades
+grade1 = float(input("Enter grade #1: "))
+grade2 = float(input("Enter grade #2: "))
+grade3 = float(input("Enter grade #3: "))
+# put them all into a new list
+grade_list = [grade1, grade2, grade3]
+
+# Do some calculations -- minimum, maximum, and average
+min_grade = 
