4e213836c9d87117df23ebb7704e28942ade4d83
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-17 18:56:28 +0000

    P2HW2 End

--- p2hw2_castroperez.py
+++ p2hw2_castroperez.py
@@ -10,4 +10,12 @@ grade3 = float(input("Enter grade #3: "))
 grade_list = [grade1, grade2, grade3]
 
 # Do some calculations -- minimum, maximum, and average
-min_grade = 
+min_grade = min(grade_list)
+max_grade = max(grade_list)
+sum_grade = sum(grade_list)
+length = len(grade_list)
+average_grade = sum(grade_list) / len(grade_list)
+
+# Reserves
+print(f"{'Lowest_Grade:':<18}{min_grade}")
+print(f"{'Average':<18}{average_grade:.2f}")
