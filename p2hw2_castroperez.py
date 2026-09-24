ec5da3b2405bfcdc7b9adadca31f35e6e5e2c77c
castropj4236 <castropj4236@student.faytechcc.edu>
2026-09-24 14:27:24 -0400

    Add files via upload

--- p2hw2_castroperez.py
+++ p2hw2_castroperez.py
@@ -6,8 +6,11 @@
 grade1 = float(input("Enter grade #1: "))
 grade2 = float(input("Enter grade #2: "))
 grade3 = float(input("Enter grade #3: "))
+grade4 = float(input("Enter grade #4: "))
+grade5 = float(input("Enter grade #5: "))
+grade6 = float(input("Enter grade #6: "))
 # put them all into a new list
-grade_list = [grade1, grade2, grade3]
+grade_list = [grade1, grade2, grade3, grade4, grade5, grade6]
 
 # Do some calculations -- minimum, maximum, and average
 min_grade = min(grade_list)
@@ -17,5 +20,8 @@ length = len(grade_list)
 average_grade = sum(grade_list) / len(grade_list)
 
 # Reserves
+print("------------Results------------")
 print(f"{'Lowest_Grade:':<18}{min_grade}")
+print(f"{'Highest_Grade:':<18}{max_grade}")
+print(f"{'Sum_of_Grade:':<18}{sum_grade}")
 print(f"{'Average':<18}{average_grade:.2f}")
